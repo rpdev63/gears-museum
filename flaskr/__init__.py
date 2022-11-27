db70b6d01c8af89dade3e08b5e89c619f980410e
zeugy <rpdev@protonmail.com>
2022-11-27 17:54:57 +0100

    liste engrenage et page détail, back end ok

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -35,8 +35,8 @@ def create_app(test_config=None):
     from . import auth
     app.register_blueprint(auth.bp)
 
-    from . import blog
-    app.register_blueprint(blog.bp)
+    from . import post
+    app.register_blueprint(post.bp)
 
     from . import gears
     app.register_blueprint(gears.bp)
--- flaskr/post.py
+++ flaskr/post.py
@@ -5,20 +5,21 @@ from werkzeug.exceptions import abort
 
 from flaskr.auth import login_required
 from flaskr.db import get_db
+bp = Blueprint('post', __name__, url_prefix='/post')
 
-bp = Blueprint('blog', __name__, url_prefix='/blog')
-
+#show all posts
 @bp.route('/')
 def index():
     db = get_db()
     posts = db.execute(
-        'SELECT p.id, title, body, created, author_id, username'
+        'SELECT p.id, title, body, created_at, author_id, username'
         ' FROM post p JOIN user u ON p.author_id = u.id'
-        ' ORDER BY created DESC'
+        ' ORDER BY created_at DESC'
     ).fetchall()
-    return render_template('blog/index.html', posts=posts)
+    return render_template('post/index.html', posts=posts)
 
 
+#create post template 
 @bp.route('/create', methods=('GET', 'POST'))
 @login_required
 def create():
@@ -40,14 +41,14 @@ def create():
                 (title, body, g.user['id'])
             )
             db.commit()
-            return redirect(url_for('blog.index'))
+            return redirect(url_for('post.index'))
 
-    return render_template('blog/create.html')
+    return render_template('post/create.html')
 
-
+#select post by ID
 def get_post(id, check_author=True):
     post = get_db().execute(
-        'SELECT p.id, title, body, created, author_id, username'
+        'SELECT p.id, title, body, created_at, author_id, username'
         ' FROM post p JOIN user u ON p.author_id = u.id'
         ' WHERE p.id = ?',
         (id,)
@@ -61,7 +62,7 @@ def get_post(id, check_author=True):
 
     return post
 
-
+#update post template
 @bp.route('/<int:id>/update', methods=('GET', 'POST'))
 @login_required
 def update(id):
@@ -85,11 +86,11 @@ def update(id):
                 (title, body, id)
             )
             db.commit()
-            return redirect(url_for('blog.index'))
+            return redirect(url_for('post.index'))
 
-    return render_template('blog/update.html', post=post)
+    return render_template('post/update.html', post=post)
 
-
+#delete
 @bp.route('/<int:id>/delete', methods=('POST',))
 @login_required
 def delete(id):
@@ -97,4 +98,4 @@ def delete(id):
     db = get_db()
     db.execute('DELETE FROM post WHERE id = ?', (id,))
     db.commit()
-    return redirect(url_for('blog.index'))
+    return redirect(url_for('post.index'))
